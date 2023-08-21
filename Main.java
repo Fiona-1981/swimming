879b4d1ae7e60454769bf3309891140808e65d92
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-08-21 17:37:10 +0100

    Pausing while I research user input for Duration data type

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
+import java.time.Duration;
 
 class Main {
   public static void main(String[] args) {
 
     Scanner mySwim = new Scanner(System.in);
-    String myEvent;
-    
-    System.out.println("Enter event:"); 
-    myEvent = mySwim.nextLine();   
-       
-    System.out.println("Your event is: " + myEvent);
-    
-    mySwim.close();
-  }
+    Scanner myTime = new Scanner(System.in);
+    System.out.println("Enter your event and time:");
+
+    String myEvent = mySwim.nextLine();
+
+    // Duration myEventTime = myTime();
+    // User input for Duration required!
+
+    System.out.println("Your event is: " + myEvent + " and your time is " + myEventTime);
+    }
 }
